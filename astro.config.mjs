5f0099d01d755d3f055c0997c6d51c83dbeee060
Radhyy <raaakb87@gmail.com>
2025-10-18 01:14:49 +0700

    Fix: Update Astro config for static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig as defineViteConfig } from 'vite';
 
 export default defineConfig({
     integrations: [tailwind()],
     output: 'static',
+    vite: defineViteConfig({
+        build: {
+            cssCodeSplit: false
+        }
+    }),
     image: {
         service: {
             entrypoint: 'astro/assets/services/sharp'
