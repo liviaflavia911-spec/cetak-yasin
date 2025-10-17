1964cb79fe6a91cb3033228c5461eab491a87717
Radhyy <raaakb87@gmail.com>
2025-10-18 01:07:00 +0700

    Update semua file terbaru

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
     integrations: [tailwind()],
